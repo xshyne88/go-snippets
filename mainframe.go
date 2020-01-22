027a10c6fb85a520a8cec1206dc35b640c4cca51
Chase Philips <chasehomedecor@gmail.com>
2020-01-22 10:46:01 -0500

    mainframe

---
+++ mainframe.go
@@ -0,0 +1,41 @@
+package main
+
+type Mainframe struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+	scoreBoard chan *Scoreboard
+}
+
+func newMainframe() *Mainframe {
+	return &Mainframe{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Mainframe) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
